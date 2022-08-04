f09f4e4735a75bec569803ff1b698dfd2d5c81aa
Lucija Krsticevic <lucija.krsticevic99@gmail.com>
2022-08-05 01:29:45 +0200

    Dodani buttoni

--- domaci01/src/index.js
+++ domaci01/src/index.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
-const App = () => (
- <div>
- <p>Hello React</p>
- </div>
-)
+
+const App = () => {
+  const Dobra = () => {
+    console.log("Dobra")
+  }
+  const Neutralna = () => {
+    console.log("Neutralna")
+  }
+  const Losa = () => {
+    console.log("Losa")
+  }
+
+  return(
+    <>
+      <h1>Ocjenite našu uslugu!</h1>
+      <button onClick={Dobra}>Dobra</button>
+      <button onClick={Neutralna}>Neutralna</button>
+      <button onClick={Losa}>Loša</button>
+    </>
+  )
+
+}
+ 
 ReactDOM.render(<App />, document.getElementById('root'))
